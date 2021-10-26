c9a0950bdfd1a0a1076c341ee742e59baa98ec50
Ilia <iliakreslov@gmail.com>
2021-10-26 19:38:00 +0300

    Realize GameFragment

--- app/src/main/java/com/example/composition/presentation/GameViewModel.kt
+++ app/src/main/java/com/example/composition/presentation/GameViewModel.kt
@@ -66,6 +66,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
         getGameSettings(level)
         startTimer()
         generateQuestion()
+        updateProgress()
     }
 
     fun chooseAnswers(number: Int) {
@@ -88,6 +89,9 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     private fun calculatePercentOfRightAnswers(): Int {
+        if (countOfQuestions == 0){
+            return 0
+        }
         return ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
     }
 
